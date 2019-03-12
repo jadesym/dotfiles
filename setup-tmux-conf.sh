25c264106cab25ac0bc0c36a52e633b12b906d35
Kevin Fu <kevin.fu808@gmail.com>
2019-03-12 00:10:54 -0700

    Implementation of adding gitbar to the tmux file
    Fixed new line issue
    
    Fixed issue where gitbar directory already exists. Wiping it

--- setup-tmux-conf.sh
+++ setup-tmux-conf.sh
@@ -1,6 +1,19 @@
-git clone https://github.com/arl/tmux-gitbar.git ~/.tmux-gitbar
-echo 'source-file "$HOME/.tmux-gitbar/tmux-gitbar.tmux"' >> .tmux.conf
+# Locally stash anything being currently worked on
+git add .
+git stash
+
+HOME_DIR="$HOME"
+TMUX_CONFIG_FILE_NAME=".tmux.conf"
+FINAL_TMUX_CONFIG_FILE_PATH="$HOME_DIR/$TMUX_CONFIG_FILE_NAME"
 
 # Copy .tmux.conf file into base user directory
-cp .tmux.conf ~/
+cp $TMUX_CONFIG_FILE_NAME $HOME_DIR
 
+# Gitbar setup for tmux, requires wiping existing gitbar directory
+rm -rf ~/.tmux-gitbar
+git clone https://github.com/arl/tmux-gitbar.git ~/.tmux-gitbar
+echo -e '\n# Setting up the gitbar functionality for tmux' >> $FINAL_TMUX_CONFIG_FILE_PATH
+echo 'source-file "$HOME/.tmux-gitbar/tmux-gitbar.tmux"' >> $FINAL_TMUX_CONFIG_FILE_PATH
+
+# Pop anything locally stashed to return back to original state
+git stash pop
