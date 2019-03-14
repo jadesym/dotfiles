90ca1505a2b369ca726dc38d8a431e6e199ce58e
Kevin Fu <kevin.fu808@gmail.com>
2019-03-13 23:30:36 -0700

    Supported dark themes

--- setup-tmux-conf.sh
+++ setup-tmux-conf.sh
@@ -5,15 +5,26 @@ git stash
 HOME_DIR="$HOME"
 TMUX_CONFIG_FILE_NAME=".tmux.conf"
 FINAL_TMUX_CONFIG_FILE_PATH="$HOME_DIR/$TMUX_CONFIG_FILE_NAME"
+TMUX_ADDONS_DIR="$HOME_DIR/.tmux"
+TOMORROW_NIGHT_THEMES_PATH="themes/tomorrow-night.tmux"
 
 # Copy .tmux.conf file into base user directory
 cp $TMUX_CONFIG_FILE_NAME $HOME_DIR
 
+# Wipe and reset the tmux addons directory
+rm -rf $TMUX_ADDONS_DIR
+mkdir $TMUX_ADDONS_DIR
+
 # Gitbar setup for tmux, requires wiping existing gitbar directory
-rm -rf ~/.tmux-gitbar
-git clone https://github.com/arl/tmux-gitbar.git ~/.tmux-gitbar
+git clone https://github.com/arl/tmux-gitbar.git "$TMUX_ADDONS_DIR/.tmux-gitbar"
 echo -e '\n# Setting up the gitbar functionality for tmux' >> $FINAL_TMUX_CONFIG_FILE_PATH
-echo 'source-file "$HOME/.tmux-gitbar/tmux-gitbar.tmux"' >> $FINAL_TMUX_CONFIG_FILE_PATH
+echo "source-file $TMUX_ADDONS_DIR/.tmux-gitbar/tmux-gitbar.tmux" >> $FINAL_TMUX_CONFIG_FILE_PATH
+
+# Copying over the dark themes we want
+mkdir "$TMUX_ADDONS_DIR/themes"
+cp $TOMORROW_NIGHT_THEMES_PATH "$TMUX_ADDONS_DIR/$TOMORROW_NIGHT_THEMES_PATH"
+echo -e '\n# Setting up the dark themes for tmux' >> $FINAL_TMUX_CONFIG_FILE_PATH
+echo "source-file $TMUX_ADDONS_DIR/$TOMORROW_NIGHT_THEMES_PATH" >> $FINAL_TMUX_CONFIG_FILE_PATH
 
 # Pop anything locally stashed to return back to original state
 git stash pop
