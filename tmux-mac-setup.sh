62ec4a3db2a78556796b5b09858d2fdbd55aa344
Kevin Fu <kevin.fu808@gmail.com>
2022-01-04 23:16:14 -0800

    Modifying OS scripts to run bash-aliases and vim setup scripts

--- tmux-mac-setup.sh
+++ tmux-mac-setup.sh
@@ -1,6 +1,10 @@
+./bash_aliases_setup.sh
+
 # Expand bash_aliases to non-interactive shells
 shopt -s expand_aliases
 source ~/.bash_aliases
+
+./vim-setup.sh
 
 brew install tmux
 ./setup-tmux-conf.sh
--- tmux-ubuntu-setup.sh
+++ tmux-ubuntu-setup.sh
@@ -1,2 +1,6 @@
+./bash_aliases_setup.sh
+
+./vim-setup.sh
+
 sudo apt-get install tmux
 ./tmux-conf-setup.sh
